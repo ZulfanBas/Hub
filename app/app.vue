03057f8bc937629c64948d19a6c7b88deedc61fd
Zulfan <166511534+ZulfanBas@users.noreply.github.com>
2024-08-29 17:47:40 +0700

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtRouteAnnouncer />
+  <NuxtLoadingIndicator />
+  <NuxtPage />
+</template>
+
+<script setup lang="ts">
+useHead({
+  bodyAttrs: {
+    class: 'bg-white dark:bg-gray-900',
+  },
+});
+</script>
